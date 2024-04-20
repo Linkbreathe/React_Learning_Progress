849a7f18df0158ce55f331fe4358945d6451a1ee
Link <956751855@qq.com>
2024-04-20 20:01:07 +0800

    Learning how to use PropTypes to limit the types of values(like ts), how to give more props in a Components

--- src/index.js
+++ src/index.js
@@ -6,10 +6,34 @@ import reportWebVitals from './reportWebVitals';
 import StarrRating from './StarrRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function Test(){
+  const [movieRating, setMovieRating] = React.useState(3);
+  return (
+    <>
+      <StarrRating maxRating={4} 
+        color={"pink"} 
+        size='20'
+        onSetRating={setMovieRating}
+      />
+      <div>This movie have gotten {movieRating} stars</div>
+    </>
+  )
+
+}
+
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     <StarrRating maxRating={10}/>
+    <StarrRating maxRating={10} color={"red"} size='20' className=""/>
+    <StarrRating maxRating={4} 
+    color={"blue"} 
+    size='20' 
+    message={['bad','ok','good','excellent']}
+    defaultRating={3}
+    className=""/>
+    <Test></Test>
   </React.StrictMode>
 );
 
