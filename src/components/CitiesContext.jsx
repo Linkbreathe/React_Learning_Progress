9662c4c4465b0aa5860ac0fc0d20223606ec7d07
Link <956751855@qq.com>
2024-04-30 01:31:17 +0800

    Fix the problem of circular dependency which occurs in the City.jsx and CitiesContext(getCity), by using the useCallback. Introduce the Lazy load in App.jsx

--- src/components/CitiesContext.jsx
+++ src/components/CitiesContext.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable default-case */
+import { useCallback } from "react";
 import {  useEffect, createContext, useContext, useReducer } from "react";
 
 const BASE_URL = "http://localhost:9000";
@@ -60,17 +61,19 @@ function CitiesContextProvider({ children }) {
     }, []);
 
     
-
-    async function getCity({id}) {
-        dispatch({type: "loading"});
-        try {
-            const res = await fetch(`${BASE_URL}/cities/${id}`);
-            const data = await res.json();
-            dispatch({type:"city/loaded",payload:data});
-        } catch (err) {
-            dispatch({type:"rejected",payload:"There was an error loading the city..."});
-        } 
-    }
+    const getCity = useCallback(
+        async function getCity({id}) {
+            if(Number(id) === currentCity.id) return;
+            dispatch({type: "loading"});
+            try {
+                const res = await fetch(`${BASE_URL}/cities/${id}`);
+                const data = await res.json();
+                dispatch({type:"city/loaded",payload:data});
+            } catch (err) {
+                dispatch({type:"rejected",payload:"There was an error loading the city..."});
+            } 
+        },[currentCity.id]
+    )
 
     async function createCity(newCity) {
         dispatch({type: "loading"});
